109e608e1eb081cdb43fe3f6b4109dc37697ec6c
pyb0924 <pyb0924@sjtu.edu.cn>
2021-05-01 22:43:53 +0800

    add Activities

--- app/src/main/java/io/bcyl/douyin/MainActivity.java
+++ app/src/main/java/io/bcyl/douyin/MainActivity.java
@@ -2,13 +2,27 @@ package io.bcyl.douyin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 public class MainActivity extends AppCompatActivity {
+    private Handler mHandler =new Handler(Looper.getMainLooper());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // jump to HomeActivity after delay
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this,HomeActivity.class));
+            }
+        },3000);
+
+
     }
 }
