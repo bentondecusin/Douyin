3d2b01fbcd6c80bea3ec3e0636ced8a8d68adb14
Benton Li <cl2597@cornell.edu>
2021-05-02 18:56:30 +0800

    Add sample videos

--- app/src/main/java/io/bcyl/douyin/Fragment/Home/HomeFragment.java
+++ app/src/main/java/io/bcyl/douyin/Fragment/Home/HomeFragment.java
@@ -1,38 +1,147 @@
 package io.bcyl.douyin.Fragment.Home;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.PagerSnapHelper;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.util.MimeTypes;
+import com.google.android.exoplayer2.util.Util;
+
 import io.bcyl.douyin.R;
 
-
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class HomeFragment extends Fragment {
+//    private PlaybackStateListener playbackStateListener;
+    private static final String TAG = HomeFragment.class.getName();
+    private PlayerView playerView;
+    private SimpleExoPlayer player;
+    private boolean playWhenReady = true;
+    private int currentWindow = 0;
+    private long playbackPosition = 0;
 
-    private PagerSnapHelper pagerSnapHelper;
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM = "param";
+
+    // TODO: Rename and change types of parameters
+    private String mParam;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param Parameter 1.
+     * @return A new instance of fragment HomeFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static HomeFragment newInstance(String param) {
+        HomeFragment fragment = new HomeFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM, param);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        playbackStateListener = new PlaybackStateListener();
+
+    }
+
+    public void onStart(){
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view=inflater.inflate(R.layout.fragment_home, container, false);
-        RecyclerView main_view=view.findViewById(R.id.main_view);
-        pagerSnapHelper=new PagerSnapHelper();
-        pagerSnapHelper.attachToRecyclerView(main_view);
-
-
-
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        playerView = view.findViewById(R.id.video_view);
         return view;
     }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+      //  hideSystemUi();
+        if ((Util.SDK_INT >= 23 /* || player == null */)) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
+    }
+
+    private void initializePlayer() {
+        player = new SimpleExoPlayer.Builder(this.getContext()).build();
+        playerView.setPlayer(player);
+
+        MediaItem mediaItem1 = new MediaItem.Builder().setUri(getString(R.string.meme1))
+                .setMimeType(MimeTypes.APPLICATION_MP4)
+                .build();
+        MediaItem mediaItem2 = new MediaItem.Builder().setUri(getString(R.string.meme2))
+                .setMimeType(MimeTypes.APPLICATION_MP4)
+                .build();
+        player.setMediaItem(mediaItem1);
+        player.addMediaItem(mediaItem2);
+        player.setPlayWhenReady(playWhenReady);
+        player.seekTo(currentWindow, playbackPosition);
+        player.prepare();
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+            playbackPosition = player.getCurrentPosition();
+            currentWindow = player.getCurrentWindowIndex();
+            playWhenReady = player.getPlayWhenReady();
+            player.release();
+            player = null;
+        }
+    }
+    @SuppressLint("InlinedApi")
+    private void hideSystemUi() {
+        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+
 }
