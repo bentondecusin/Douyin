9561032d084b44a2172f541a1da9ad79817bcb3f
pyb0924 <pyb0924@sjtu.edu.cn>
2021-05-02 13:12:50 +0800

    finished navigation bar

--- app/src/main/java/io/bcyl/douyin/HomeActivity.java
+++ app/src/main/java/io/bcyl/douyin/HomeActivity.java
@@ -1,30 +1,98 @@
 package io.bcyl.douyin;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import io.bcyl.douyin.Fragment.Add.AddFragment;
+import io.bcyl.douyin.Fragment.Home.HomeFragment;
+import io.bcyl.douyin.User.UserFragment;
 
 public class HomeActivity extends AppCompatActivity {
-
+    private Fragment mFragmentHome,mFragmentAdd,mFragmentUser;
+    final int FRAGMENT_HOME =0, FRAGMENT_ADD =1, FRAGMENT_USER =2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+
+        initView();
     }
 
+    private void initView() {
+        BottomNavigationView mBottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation_view);
+
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_home:
+                        showFragment(FRAGMENT_HOME);
+                        return true;
+                    case R.id.menu_add:
+                        showFragment(FRAGMENT_ADD);
+                        return true;
+                    case R.id.menu_user:
+                        showFragment(FRAGMENT_USER);
+                        return true;
+                }
+                return false;
+            }
+        });
+        showFragment(FRAGMENT_HOME);
+    }
+
+    private void showFragment(int index) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        hideFragment(transaction);
+        switch (index) {
+            case FRAGMENT_HOME:
+                if (mFragmentHome == null) {
+                    mFragmentHome = new HomeFragment();
+                    transaction.add(R.id.main_container, mFragmentHome);
+                } else {
+                    transaction.show(mFragmentHome);
+                }
+                break;
+            case FRAGMENT_ADD:
+                if (mFragmentAdd == null) {
+                    mFragmentAdd = new AddFragment();
+                    transaction.add(R.id.main_container, mFragmentAdd);
+                } else {
+                    transaction.show(mFragmentAdd);
+                }
+                break;
+            case FRAGMENT_USER:
+                if (mFragmentUser == null) {
+                    mFragmentUser = new UserFragment();
+                    transaction.add(R.id.main_container, mFragmentUser);
+                } else {
+                    transaction.show(mFragmentUser);
+
+                }
+                break;
+        }
+        transaction.commit();
+    }
+
+    //隐藏
+    private void hideFragment(FragmentTransaction transaction) {
+        if (mFragmentHome != null) {
+            transaction.hide(mFragmentHome);
+        }
+        if (mFragmentAdd != null) {
+            transaction.hide(mFragmentAdd);
+        }
+        if (mFragmentUser != null) {
+            transaction.hide(mFragmentUser);
+        }
+    }
 }
