962f163c2039a7cb8b539d1b14df05444028a154
Benton Li <cl2597@cornell.edu>
2021-05-09 03:16:31 +0800

    New identifier

--- app/src/main/java/io/bcyl/douyin/Utils/Constants.java
+++ app/src/main/java/io/bcyl/douyin/Utils/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     public static final String BASE_URL = "https://api-sjtu-camp-2021.bytedance.com/homework/invoke/";
     public static final String token = "U0pUVS1ieXRlZGFuY2UtYW5kcm9pZA==";
 
-    public static final String STUDENT_ID = "MacroHard2";
+    public static final String STUDENT_ID = "MacroHard3";
     public static final String USER_NAME = "Nobody";
     public static final String IDENTIFIER = "MacroHard2";
     public static final String DELIM = ",,,";
