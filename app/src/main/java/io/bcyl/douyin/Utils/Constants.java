7c6c630b8d4a3dfc668d7a039bf83d298bc5840e
Benton Li <cl2597@cornell.edu>
2021-05-08 18:24:08 +0800

    Update Constants.java

--- app/src/main/java/io/bcyl/douyin/Utils/Constants.java
+++ app/src/main/java/io/bcyl/douyin/Utils/Constants.java
@@ -4,9 +4,9 @@ public class Constants {
     public static final String BASE_URL = "https://api-sjtu-camp-2021.bytedance.com/homework/invoke/";
     public static final String token = "U0pUVS1ieXRlZGFuY2UtYW5kcm9pZA==";
 
-    public static final String STUDENT_ID = "MacroHard1";
+    public static final String STUDENT_ID = "MacroHard2";
     public static final String USER_NAME = "Nobody";
-    public static final String IDENTIFIER = "MacroHard1";
-    public static final String DELIM = "@#$%";
+    public static final String IDENTIFIER = "MacroHard2";
+    public static final String DELIM = ",,,";
 
 }
